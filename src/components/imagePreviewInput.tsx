fa0d8a42292469079066d1cc3eac4de92c56ef00
ignaciovq <56691619+ignaciovq@users.noreply.github.com>
2023-04-28 00:44:35 -0300

    feat: Add button for removing single image
    Added button to remove the currently displayed image from the file list.

--- src/components/imagePreviewInput.tsx
+++ src/components/imagePreviewInput.tsx
@@ -4,10 +4,11 @@ import { Splide, SplideSlide } from '@splidejs/react-splide'
 import Image from 'next/image'
 import '@splidejs/react-splide/css'
 
-const ImagePreviewInput = () => {
+export const ImagePreviewInput = () => {
   const {
     formRef,
-    inputRef, mangaPages,
+    inputRef,
+    mangaPages,
     uploadPreview,
     error,
     onDragOver,
@@ -15,26 +16,28 @@ const ImagePreviewInput = () => {
     onDrop,
     onChange,
     onClick,
+    removeFile,
     clearFiles
   } = useImageInput()
 
   return (
-    <form onDrop={onDrop} onDragOver={onDragOver} onDragLeave={onDragLeave} ref={formRef} className=' flex flex-col grow justify-center items-center border-4 border-dashed rounded-xl border-transparent '>
+    <form onDrop={onDrop} onDragOver={onDragOver} onDragLeave={onDragLeave} ref={formRef} className='w-screen md:w-1/2 overflow-x-hidden flex flex-col grow justify-center items-center border-4 border-dashed rounded-xl border-transparent'>
 
-      <div className='w-4/5 sm:w-[35rem] bg-gray-100 rounded-xl flex flex-col grow justify-center items-center gap-4 p-8'>
+      <div className='w-full md:w-4/6 bg-gray-400 rounded-xl flex flex-col grow justify-center items-center gap-4 p-8'>
         {uploadPreview.length > 0
           ? (
             <Splide>
               {uploadPreview.map((preview, index) => {
                 return (
-                  <SplideSlide className='h-full' key={preview}>
+                  <SplideSlide className='relative h-fit' key={preview}>
+                    <button onClick={() => removeFile(index)} className='text-4xl absolute right-4 top-5 bg-gray-400/40 text-gray-600 hover:text-black hover:bg-gray-400/75 rounded-xl h-fit leading-[0]'>x</button>
                     <Image className='h-full object-contain' src={preview} alt={mangaPages[index].name} width={500} height={1000} />
                   </SplideSlide>
                 )
               })}
             </Splide>
             )
-          : <Image className='h-48 rounded-xl object-contain' src='/images/upload.svg' alt='Upload' width={500} height={1000} />}
+          : <Image className='h-48 rounded-xl object-contain sepia' src='/images/upload.svg' alt='Upload' width={500} height={1000} />}
       </div>
 
       <div className='flex flex-col gap-4 m-4 text-xl'>
@@ -44,7 +47,7 @@ const ImagePreviewInput = () => {
 
         <div className='flex justify-between items-center gap-2'>
           <button className='text-blue-400 border-2 rounded-xl border-blue-400 hover:text-blue-200 hover:border-blue-200 self-start py-2 p-4' onClick={clearFiles}>Clear</button>
-          <button className='bg-blue-400 hover:bg-blue-200 text-white text-2xl rounded-xl px-8 py-2 self-end ' type='submit'>Translate</button>
+          <button className='bg-blue-400 hover:bg-blue-200 text-white text-2xl rounded-xl px-8 py-2 self-end' type='submit'>Translate</button>
         </div>
 
         {error !== null && <p className='text-red-500 self-center'>{error}</p>}
