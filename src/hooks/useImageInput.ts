efe1e3db517f75df87717af6d1504c7e5db61b4e
Jean Pierre <jeanpierre.webdev@gmail.com>
2023-04-29 02:08:59 -0500

    fix: rename file name and refactor logic

--- src/hooks/useImageInput.ts
+++ src/hooks/useImageInput.ts
@@ -1,5 +1,10 @@
 import { useRef, useState } from 'react'
 import { createPreview } from '@/utils/createImagePreview'
+import { getOcrMangaData } from '@/services/ocr'
+import { translate } from '@/services/translate'
+import { Manga } from '@/types'
+import { useMangaStore } from '@/store/manga.store'
+import { useRouter } from 'next/router'
 
 export interface MangaFile {
   name: string
@@ -8,10 +13,12 @@ export interface MangaFile {
 
 export function useImageInput () {
   const [mangaPages, setMangaPages] = useState<MangaFile[]>([])
-  const [uploadPreview, setUploadPreview] = useState<string[]>([])
+  const [uploadPreview, setUploadPreview] = useState<Manga[]>([])
   const [error, setError] = useState<string | null>(null)
-  const formRef = useRef(null)
-  const inputRef = useRef(null)
+  const formRef = useRef<HTMLFormElement>(null)
+  const inputRef = useRef<HTMLInputElement>(null)
+  const [setMangas, setIsLoading] = useMangaStore(state => [state.setMangas, state.setIsLoading])
+  const router = useRouter()
 
   const saveAndPreview = (files: File[]) => {
     const newMangas = files.map((file) => ({ name: file.name, file }))
@@ -20,8 +27,7 @@ export function useImageInput () {
     for (const file of files) {
       createPreview(file)
         .then((preview) => {
-          console.log(preview)
-          setUploadPreview(prev => [...prev, preview])
+          setUploadPreview(prev => [...prev, { preview, results: [], name: file.name }])
         })
         .catch((err) => {
           setError(err.message)
@@ -30,7 +36,7 @@ export function useImageInput () {
   }
 
   const removeFormBorder = () => {
-    const { current } = formRef as unknown as { current: HTMLFormElement }
+    const { current } = formRef
     if (current === null) return
     current.classList.remove('border-gray-400')
     current.classList.add('border-transparent')
@@ -38,7 +44,7 @@ export function useImageInput () {
 
   const onDragOver = (e: React.DragEvent<HTMLFormElement>) => {
     e.preventDefault()
-    const { current } = formRef as unknown as { current: HTMLFormElement }
+    const { current } = formRef
     if (current === null) return
     current.classList.remove('border-transparent')
     current.classList.add('border-gray-400')
@@ -63,7 +69,7 @@ export function useImageInput () {
 
   const onClick = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault()
-    const { current } = inputRef as unknown as { current: HTMLInputElement }
+    const { current } = inputRef
     if (current === null) return
     current.click()
   }
@@ -71,22 +77,54 @@ export function useImageInput () {
   const removeFile = (index: number) => {
     setMangaPages(prev => prev.filter((manga, i) => i !== index))
     setUploadPreview(prev => prev.filter((manga, i) => i !== index))
-    const { current } = inputRef as unknown as { current: HTMLInputElement }
-    current.value = ''
+    if (inputRef.current === null) return
+    inputRef.current.value = ''
   }
 
   const clearFiles = () => {
     setMangaPages([])
     setUploadPreview([])
     setError(null)
-    const { current } = inputRef as unknown as { current: HTMLInputElement }
-    current.value = ''
+    if (inputRef.current === null) return
+    inputRef.current.value = ''
   }
 
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
-    console.log('submit')
-    /* TODO: implement POST files to server */
+
+    try {
+      setIsLoading(true)
+      const imagesOcrPromises = mangaPages.map(async (manga) => {
+        const data = await getOcrMangaData(manga.file)
+        return data
+      })
+
+      const imagesOcr = await Promise.all(imagesOcrPromises)
+
+      const imagesOcrWithTraduction = imagesOcr.map(async ({ filename, language, result }) => {
+        const traductionPromises = result.map(async ({ text, coordinates }) => {
+          const { result } = await translate({ text, fromLanguage: 'ja', toLanguage: 'es' })
+          return { text: result, coordinates }
+        })
+        const traductions = await Promise.all(traductionPromises)
+        return { filename, language, result: traductions }
+      })
+
+      const results = await Promise.all(imagesOcrWithTraduction)
+
+      const mangas = uploadPreview.map(manga => {
+        const mangaOcr = results.find(result => result.filename === manga.name)
+        if (mangaOcr === undefined) return manga
+        return { ...manga, results: mangaOcr.result }
+      })
+
+      setMangas(mangas)
+      void router.push('/result')
+    } catch (error) {
+      console.log('error enviando', error)
+    } finally {
+      setIsLoading(false)
+    }
   }
 
   return {
@@ -102,6 +140,6 @@ export function useImageInput () {
     onClick,
     removeFile,
     clearFiles,
-    onSubmit
+    handleSubmit
   }
 }
