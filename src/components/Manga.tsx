96ab61102404cc41cd8cba1b80a130aa7dd1a4b6
Jean Pierre <jeanpierre.webdev@gmail.com>
2023-04-29 02:20:26 -0500

    refactor: add setFontSize function

--- src/components/Manga.tsx
+++ src/components/Manga.tsx
@@ -24,17 +24,25 @@ export function Manga ({ preview, results, index }: MangaProps) {
     })
   }
 
+  const handleFontSize = (n: number) => {
+    const number = fontSize + n
+
+    if (number < 1) return
+
+    setFontSize(number)
+  }
+
   return (
     <>
       <div className='flex justify-center gap-4 w-fit mx-auto items-center mt-14 mb-8 bg-gray-200 rounded-md px-4 py-3'>
-        <p className='text-gray-800 font-bold'>Aumentar tamaño de letra: {fontSize}px</p>
-        <button className='bg-blue-600 hover:bg-blue-700 transition-colors text-white rounded-md w-10 aspect-square flex justify-center items-center' onClick={() => setFontSize(fontSize - 1)}>
+        <p className='text-gray-800 font-bold w-44'>Change font size: {fontSize}px</p>
+        <button className='bg-blue-600 hover:bg-blue-700 transition-colors text-white rounded-md w-10 aspect-square flex justify-center items-center' onClick={() => handleFontSize(-1)}>
           <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth={1.5} stroke='currentColor' className='w-[18px] h-[18px]'>
             <path strokeLinecap='round' strokeLinejoin='round' d='M19.5 12h-15' />
           </svg>
 
         </button>
-        <button className='bg-blue-600 hover:bg-blue-700 transition-colors text-white rounded-md w-10 aspect-square flex justify-center items-center' onClick={() => setFontSize(fontSize + 1)}>
+        <button className='bg-blue-600 hover:bg-blue-700 transition-colors text-white rounded-md w-10 aspect-square flex justify-center items-center' onClick={() => handleFontSize(1)}>
           <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth={1.5} stroke='currentColor' className='w-[18px] h-[18px]'>
             <path strokeLinecap='round' strokeLinejoin='round' d='M12 4.5v15m7.5-7.5h-15' />
           </svg>
@@ -52,7 +60,7 @@ export function Manga ({ preview, results, index }: MangaProps) {
           )
         })}
       </div>
-      <button onClick={async () => handleDownload(`manga-${index + 1}`)} className='bg-blue-600 hover:bg-blue-700 text-white text-2xl rounded px-8 py-2 self-end transition-colors w-fit block mx-auto mt-4'>Descargar</button>
+      <button onClick={async () => handleDownload(`manga-${index + 1}`)} className='bg-blue-600 hover:bg-blue-700 text-white text-2xl rounded px-8 py-2 self-end transition-colors w-fit block mx-auto mt-4'>Download</button>
     </>
   )
 };
