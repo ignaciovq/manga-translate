f4b4db49db183be185d349bf299c1f0debcee41b
Jean Pierre <jeanpierre.webdev@gmail.com>
2023-04-29 02:09:37 -0500

    refactor: change loading states and add links

--- src/components/imagePreviewInput.tsx
+++ src/components/imagePreviewInput.tsx
@@ -1,8 +1,9 @@
-'use client'
-import { useImageInput } from '../hooks/useImageInput'
+import { useImageInput } from '@/hooks/useImageInput'
 import { Splide, SplideSlide } from '@splidejs/react-splide'
 import Image from 'next/image'
 import '@splidejs/react-splide/css'
+import { Loader } from './Loader'
+import { useMangaStore } from '@/store/manga.store'
 
 export const ImagePreviewInput = () => {
   const {
@@ -17,45 +18,50 @@ export const ImagePreviewInput = () => {
     onChange,
     onClick,
     removeFile,
-    clearFiles
+    clearFiles,
+    handleSubmit
   } = useImageInput()
+  const [isLoading] = useMangaStore(state => [state.isLoading])
 
   return (
-    <form onDrop={onDrop} onDragOver={onDragOver} onDragLeave={onDragLeave} ref={formRef} className='w-screen md:w-1/2 overflow-x-hidden flex flex-col grow justify-center items-center border-4 border-dashed rounded-xl border-transparent'>
+    <>
+      <form onSubmit={handleSubmit} onDrop={onDrop} onDragOver={onDragOver} onDragLeave={onDragLeave} ref={formRef} className='w-screen md:w-1/2 overflow-x-hidden flex flex-col grow justify-center items-center border-4 border-dashed rounded-xl border-transparent' style={{ pointerEvents: isLoading ? 'none' : 'auto' }}>
 
-      <div className='w-full md:w-4/6 bg-gray-400 rounded-xl flex flex-col grow justify-center items-center gap-4 p-8'>
-        {uploadPreview.length > 0
-          ? (
-            <Splide>
-              {uploadPreview.map((preview, index) => {
-                return (
-                  <SplideSlide className='relative h-fit' key={preview}>
-                    <button onClick={() => removeFile(index)} className='text-4xl absolute right-4 top-5 bg-gray-400/40 text-gray-600 hover:text-black hover:bg-gray-400/75 rounded-xl h-fit leading-[0]'>x</button>
-                    <Image className='h-full object-contain' src={preview} alt={mangaPages[index].name} width={500} height={1000} />
-                  </SplideSlide>
-                )
-              })}
-            </Splide>
-            )
-          : <Image className='h-48 rounded-xl object-contain sepia' src='/images/upload.svg' alt='Upload' width={500} height={1000} />}
-      </div>
+        <div className='w-full md:w-4/6 bg-gray-400 rounded-xl flex flex-col grow justify-center items-center gap-4 p-8 relative'>
+          {uploadPreview.length > 0
+            ? (
+              <Splide>
+                {uploadPreview.map(({ preview, results }, index) => {
+                  return (
+                    <SplideSlide className='relative h-fit' key={preview}>
+                      <button onClick={() => removeFile(index)} className='text-4xl absolute right-4 top-5 bg-gray-400/40 text-gray-600 hover:text-black hover:bg-gray-400/75 rounded-xl h-fit leading-[0]'>x</button>
+                      <Image className='h-full object-contain' src={preview} alt={mangaPages[index].name} width={500} height={1000} />
+                    </SplideSlide>
+                  )
+                })}
 
-      <div className='flex flex-col gap-4 m-4 text-xl'>
+              </Splide>
+              )
+            : <Image className='h-48 rounded-xl object-contain sepia w-auto' src='/images/upload.svg' alt='Upload' width={500} height={1000} priority />}
+          {isLoading && <Loader />}
 
-        <label><button className='text-blue-400 hover:text-blue-600' onClick={onClick}>Select</button> or drop your files to upload them</label>
-        <input className='hidden' ref={inputRef} type='file' placeholder='Manga' accept='image/*' multiple onChange={onChange} />
-
-        <div className='flex justify-between items-center gap-2'>
-          <button className='text-blue-400 border-2 rounded-xl border-blue-400 hover:text-blue-200 hover:border-blue-200 self-start py-2 p-4' onClick={clearFiles}>Clear</button>
-          <button className='bg-blue-600 hover:bg-blue-400 text-white text-2xl rounded-xl px-8 py-2 self-end transition-colors' type='submit'>Translate</button>
         </div>
 
-        {error !== null && <p className='text-red-500 self-center'>{error}</p>}
+        <div className='flex flex-col gap-4 m-4 text-xl'>
 
-      </div>
+          <label><button className='text-blue-400 hover:text-blue-600' onClick={onClick}>Select</button> or drop your files to upload them</label>
+          <input className='hidden' ref={inputRef} type='file' placeholder='Manga' accept='image/*' multiple onChange={onChange} max={8} />
 
-    </form>
+          <div className='flex justify-between items-center gap-2'>
+            <button className='text-blue-400 border-2 rounded-xl border-blue-400 hover:text-blue-200 hover:border-blue-200 self-start py-2 p-4' onClick={clearFiles}>Clear</button>
+            <button className='bg-blue-600 hover:bg-blue-400 text-white text-2xl rounded-xl px-8 py-2 self-end transition-colors' type='submit'>Translate</button>
+          </div>
+
+          {error !== null && <p className='text-red-500 self-center'>{error}</p>}
+
+        </div>
+
+      </form>
+    </>
   )
 }
-
-export default ImagePreviewInput
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,26 @@
-import ImagePreviewInput from '@/components/imagePreviewInput'
+import { ImagePreviewInput } from '@/components/imagePreviewInput'
+import Head from 'next/head'
+import Link from 'next/link'
 
 export default function Manga () {
   return (
-    <section className='h-full min-h-screen w-screen overflow-x-hidden flex flex-col gap-y-4 items-center p-4'>
-      <header>
-        <h1 className='text-3xl'>Translate your Manga</h1>
-      </header>
-      <ImagePreviewInput />
-    </section>
+    <>
+      <Head>
+        <title>Home | Manga Translate</title>
+      </Head>
+      <main className='h-full min-h-screen w-screen overflow-x-hidden flex flex-col gap-y-4 items-center p-4'>
+        <header>
+          <h1 className='text-3xl'>Translate your Manga</h1>
+        </header>
+        <ImagePreviewInput />
+
+        <Link href='/result' className='flex items-center gap-2 text-center font-bold mt-4 hover:border-white/100 b border-b border-white/0 transition-colors'>Got any preview results? Go check them out!
+          <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth={1.5} stroke='currentColor' className='w-5 h-5'>
+            <path strokeLinecap='round' strokeLinejoin='round' d='M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3' />
+          </svg>
+
+        </Link>
+      </main>
+    </>
   )
 }
